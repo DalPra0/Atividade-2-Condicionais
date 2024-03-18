b5625d921ace867a2dbb333d7eb2660fca1f90c5
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2024-03-18 11:35:50 -0300

    Exercicio7.py

--- Exercicio7.py
+++ Exercicio7.py
@@ -14,9 +14,3 @@ peso = float(input("Digite seu peso (em kg): "))
 
 # Verifica se o visitante está liberado para andar na montanha russa
 print(verificar_acesso_hopihari(idade, peso))
-
-##Comece com uma entrada onde o usuário fornece a idade e o peso.
-##Em seguida, temos um losango representando uma decisão com duas opções: idade maior que 15 anos e peso inferior ou igual a 120 kg.
-##e a idade for maior que 15 anos e o peso for inferior ou igual a 120 kg, o visitante está liberado para andar na montanha russa.
-##Se não, o visitante não está liberado para andar na montanha russa.
-##A saída é exibida na tela com a mensagem correspondente.
