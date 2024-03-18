11299972ff0ceb74d29266c76b7730f47ae8f102
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2024-03-18 11:36:05 -0300

    Exercicio8.py

--- Exercicio8.py
+++ Exercicio8.py
@@ -15,10 +15,3 @@ idade = int(input("Digite sua idade: "))
 
 # Verifica a situação eleitoral
 print("Sua situação eleitoral é:", verificar_situacao_eleitoral(idade))
-
-##Comece com uma entrada onde o usuário fornece a idade.
-##Em seguida, temos um losango representando uma decisão com três opções: idade menor que 16 anos, idade entre 16 e 18 anos (inclusive) ou idade maior ou igual a 65 anos.
-##Se a idade for menor que 16 anos, a pessoa é classificada como "Não eleitor".
-##Se a idade estiver entre 16 e 18 anos (inclusive) ou for maior ou igual a 65 anos, a pessoa é classificada como "Eleitor facultativo".
-##Se a idade estiver fora dessas faixas, a pessoa é classificada como "Eleitor obrigatório".
-##A saída é exibida na tela com a situação eleitoral correspondente.
