3d7975b201a33df9c293b9103d10a405637b3413
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2024-03-18 11:35:27 -0300

    Exercicio9.py

--- Exercicio9.py
+++ Exercicio9.py
@@ -14,11 +14,3 @@ tempo_servico = int(input("Digite seu tempo de serviço (em anos): "))
 
 # Verifica se o trabalhador pode se aposentar
 print(verificar_aposentadoria(idade, tempo_servico))
-
-##Comece com duas entradas onde o usuário fornece a idade e o tempo de serviço.
-##Em seguida, temos um losango representando uma decisão com três opções: idade maior ou igual a 65 anos, tempo de serviço maior ou igual a 30 anos e uma combinação de idade maior ou igual a 60 anos e tempo de serviço maior ou igual a 25 anos.
-##Se a idade for maior ou igual a 65 anos, o trabalhador pode se aposentar.
-##Se o tempo de serviço for maior ou igual a 30 anos, o trabalhador pode se aposentar.
-##Se a idade for maior ou igual a 60 anos e o tempo de serviço for maior ou igual a 25 anos, o trabalhador pode se aposentar.
-##Se nenhuma das condições acima for atendida, o trabalhador não pode se aposentar.
-##A saída é exibida na tela com a mensagem correspondente à possibilidade de aposentadoria.
