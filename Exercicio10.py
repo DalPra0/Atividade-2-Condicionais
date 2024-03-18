dbf3c22c8521de914ad73a104686ed23d75ac3ae
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2024-03-18 11:36:25 -0300

    Exercicio10.py

--- Exercicio10.py
+++ Exercicio10.py
@@ -27,9 +27,3 @@ codigo_universidade = int(input("Digite o código da universidade (1 para PUCPR,
 
 # Verifica a situação do estudante
 print("Situação do estudante:", verificar_situacao_estudante(nota1, nota2, codigo_universidade))
-
-##Comece com três entradas onde o usuário fornece a primeira nota, a segunda nota e o código da universidade.
-##Em seguida, temos um losango representando uma decisão com duas opções: código da universidade igual a 1 (PUCPR) e código da universidade igual a 2 (UNICAMP).
-##Para o código da universidade igual a 1 (PUCPR), verificamos a média e decidimos se o estudante está aprovado, em exame ou reprovado com base nas diretrizes da instituição.
-##Para o código da universidade igual a 2 (UNICAMP), verificamos a média e decidimos se o estudante está aprovado ou em exame com base nas diretrizes da instituição.
-##A saída é exibida na tela com a situação do estudante.
